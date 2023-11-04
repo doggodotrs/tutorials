c35d77b19be4183641a26a6e59387c8f55851404
doggodotrs <143339317+doggodotrs@users.noreply.github.com>
2023-11-04 14:48:04 -0600

    Add files via upload

---
+++ Series 01/Tutorial 01x17/if.rs
@@ -0,0 +1,16 @@
+fn main() {
+    // conditions
+    // let (x, y) = (1, 2);
+    // let (x, y) = (2, 1);
+    let (x, y) = (1, 1);
+
+    // if expression
+    if x > y {
+        println!("{x} > {y}");
+    } else if x < y {
+        println!("{x} < {y}");
+    } else {
+        println!("{x} == {y}");
+    }
+
+}
